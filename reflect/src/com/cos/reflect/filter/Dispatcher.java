e70d605234a658f74a1d3b864b1d9628e2f16eda
SungJaeCho <liadus@naver.com>
2021-05-02 08:55:28 +0900

    리플렉션 메서드에 파라미터 주입

--- reflect/src/com/cos/reflect/filter/Dispatcher.java
+++ reflect/src/com/cos/reflect/filter/Dispatcher.java
@@ -1,6 +1,7 @@
 package com.cos.reflect.filter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
@@ -19,7 +20,9 @@ import com.cos.reflect.anno.RequestMapping;
 import com.cos.reflect.controller.UserController;
 
 //분기 시키기 (Router의 역할)
+//함수의 실행 직전과 직후를 관리할수 있음 AOP
 public class Dispatcher implements Filter{
+	boolean isMatching = false;
 
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
@@ -56,27 +59,28 @@ public class Dispatcher implements Filter{
 //			}
 //		}
 		
+		
 		for (Method method : methods) { // 4바퀴 (join, login, user, hello)
 			Annotation annotation = method.getDeclaredAnnotation(RequestMapping.class);
 			RequestMapping requestMapping = (RequestMapping) annotation; //다운 캐스팅 이렇게하면 바로 RequestMapping을 new한것보다 사용할수 있는게 많음
 //			System.out.println(requestMapping.value());
-			
 			if(requestMapping.value().equals(endPoint)) {
+				isMatching = true;
 				try {
 					//파라미터 분석
 					Parameter[] params = method.getParameters();
 					String path = null;
 					if(params.length != 0) {
 //						System.out.println("params[0].getType() : " + params[0].getType());
-						Object dtoInstance = params[0].getType().newInstance();
+						Object dtoInstance = params[0].getType().newInstance(); // 해당 오브젝트를 리플렉션해서 set함수 호출
 //						String username = request.getParameter("username");
 //						String password = request.getParameter("password");
 //						System.out.println("username : " + username);
 //						System.out.println("password : " + password);
-						Enumeration<String> keys = request.getParameterNames();//키값 
-						// keys값을 변형 ex:username => setUsername
 						
-						path = "/";
+						// keys값을 변형 ex:username => setUsername
+						setData(dtoInstance, request);
+						path = (String) method.invoke(userController, dtoInstance);
 					} else {
 						path = (String) method.invoke(userController);
 					}
@@ -89,9 +93,45 @@ public class Dispatcher implements Filter{
 				break;
 			}
 		}
+		if(!isMatching) {
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.println("잘못된 주소 요청입니다. 404에러");
+			out.flush();
+		}
+	}
+	
+	private <T> void setData(T instance, HttpServletRequest request) {
+		Enumeration<String> keys = request.getParameterNames();//키값 
+		while(keys.hasMoreElements()) { //값이 있으면 열거형 타입
+			String key = (String) keys.nextElement();
+			String methodKey = keyToMethodKey(key);
+			
+			Method[] methods = instance.getClass().getDeclaredMethods(); //선언된 메서드 dto들
+			for (Method method : methods) { //dto내의 함수 toString ,set, get들
+				if(method.getName().equals(methodKey)) {
+					try {
+						if("id".equals(key)) { //id는 int형임
+							method.invoke(instance, Integer.valueOf(request.getParameter(key)));
+						} else {
+							method.invoke(instance, request.getParameter(key));
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					break;
+				}
+			}
+			
+		}
+	}
+	
+	private String keyToMethodKey(String key) {
+		String firstKey = "set";
+		String upperKey = key.substring(0, 1).toUpperCase();
+		String remainKey = key.substring(1);
 		
-		
-		
+		return firstKey + upperKey + remainKey;
 	}
 
 }
